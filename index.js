0b2378f819b5823d782fe3f333a157ab0f147b65
af <agustinfiorenza@gmail.com>
2017-10-11 19:10:26 -0300

    final merge conflict fixes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {AppContainer} from 'react-hot-loader'
 
 class App extends React.Component {
   constructor(props) {
