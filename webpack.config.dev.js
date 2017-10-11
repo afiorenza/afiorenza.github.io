7f80da9e87a48a490231b5448cbc52e845ee99e5
af <agustinfiorenza@gmail.com>
2017-10-11 19:09:25 -0300

    fix conflicts

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,17 +10,7 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
-<<<<<<< HEAD:webpack.config.js
   devtool: 'inline-source-map',
-=======
-  resolve: {
-    extensions: ['', '.js']
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true
-  },
->>>>>>> 768d0ded8a261dc3665207ad014776da0fa19ec1:webpack.config.dev.js
   plugins: [
     new webpack.NoEmitOnErrorsPlugin()
   ],
@@ -29,15 +19,11 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-<<<<<<< HEAD:webpack.config.js
         loaders: "babel-loader",
         include: __dirname,
         query: {
           presets: ['es2015', 'react', 'react-hmre']
         }
-=======
-        loaders: ['babel-loader', 'react', 'react-hot-loader/webpack']
->>>>>>> 768d0ded8a261dc3665207ad014776da0fa19ec1:webpack.config.dev.js
       }
     ]
   }
