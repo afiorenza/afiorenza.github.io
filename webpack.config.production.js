5b9a528e5339fafd92f256ee64a5a6565406b6aa
af <agustinfiorenza@gmail.com>
2017-10-12 11:50:57 -0300

    modify webpack

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -54,7 +54,7 @@ module.exports = {
       },
       {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
+        loader: 'file-loader'
       }
     ]
   },
