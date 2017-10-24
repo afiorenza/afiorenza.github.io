9fb1aa748c80b1ebf5e313bbd4ab590fa0dda2bd
af <agustinfiorenza@gmail.com>
2017-10-23 22:42:43 -0300

    deploy

--- src/components/headers/header-component.jsx
+++ src/components/headers/header-component.jsx
@@ -5,7 +5,9 @@ import SocialBadges from 'components/social-badges';
 
 export let Header = () =>
   <div className="Header">
-    <span className="Header--email text-bold"><i className="fa fa-envelope-o" aria-hidden="true"></i> agustinfiorenza@gmail.com</span>
+    <span className="Header--email text-bold"><i className="fa fa-envelope-o" aria-hidden="true"></i>
+      <a href="mailto:agustinfiorenza@gmail.com"> agustinfiorenza@gmail.com</a> 
+    </span>
     <span className="Header--social-media-wrapper">
       <SocialBadges type="linkedin" link="https://www.linkedin.com/in/agustin-fiorenza-88869892/" className="Header--social-media-icons"/>
       <SocialBadges type="github" link="https://github.com/afiorenza" className="Header--social-media-icons"/>
