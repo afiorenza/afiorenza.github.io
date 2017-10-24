807dc8bd64424388c7fae40c6cb49393f5cdbd6d
af <agustinfiorenza@gmail.com>
2017-10-24 10:56:59 -0300

    deploy

--- src/components/headers/header-component.jsx
+++ src/components/headers/header-component.jsx
@@ -5,9 +5,9 @@ import SocialBadges from 'components/social-badges';
 
 export let Header = () =>
   <div className="Header">
-    <a href="mailto:agustinfiorenza@gmail.com"><i className="Header--email fa fa-envelope-o" aria-hidden="true"></i></a>
-    <SocialBadges type="linkedin" link="https://www.linkedin.com/in/agustin-fiorenza-88869892/" className="Header--social-media-icons"/>
-    <SocialBadges type="github" link="https://github.com/afiorenza" className="Header--social-media-icons"/>
+    <SocialBadges type="envelope-o" link="mailto:agustinfiorenza@gmail.com" className="Header--social-media-icons" />
+    <SocialBadges type="linkedin" link="https://www.linkedin.com/in/agustin-fiorenza-88869892/" className="Header--social-media-icons" blank />
+    <SocialBadges type="github" link="https://github.com/afiorenza" className="Header--social-media-icons" blank/>
   </div>
 
 export default Header;
--- src/components/social-badges/social-badges-component.jsx
+++ src/components/social-badges/social-badges-component.jsx
@@ -4,15 +4,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export let SocialBadges = ({className, type, link}) =>
-  <a className={className} target="_blank" href={link}>
+export let SocialBadges = ({className, type, link, blank}) =>
+  <a className={className} target={blank && '_blank'} href={link}>
     <i className={`SocialBadges fa fa-${type}`} aria-hidden="true"></i>
   </a>
 
 SocialBadges.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
-  link: PropTypes.string
+  link: PropTypes.string,
+  blank: PropTypes.bool
 }
 
 export default SocialBadges;
