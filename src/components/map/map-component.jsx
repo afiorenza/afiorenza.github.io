f3cdc4eaeb3611f8a0faf738ad9beece1de5910e
af <agustinfiorenza@gmail.com>
2017-11-06 10:47:56 -0300

    deploy

--- src/components/map/map-component.jsx
+++ src/components/map/map-component.jsx
@@ -4,7 +4,7 @@ import {withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps
 
 export const MapComponent = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDoLwUFF-bxfdb7ehNxugr87TvKvegI-Uo&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `500px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
